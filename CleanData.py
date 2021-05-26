c9a95b8bbbb4137f3ecf59e30d2aee49f3d2cbba
Gian Carlo Torres <geanu@outlook.com>
2021-05-25 21:30:58 -0400

    main

--- CleanData.py
+++ CleanData.py
@@ -1,5 +1,8 @@
 import pandas as pd
-from ImportData import df_sheet1, df_sheet2
+#from ImportData import df_sheet1, df_sheet2
+
+df_sheet1 = pd.read_csv('data/verifAll.csv')
+df_sheet2 = pd.read_csv('data/verifP4.csv')
 
 
 def s1_df1(dt=df_sheet1):
@@ -8,16 +11,17 @@ def s1_df1(dt=df_sheet1):
     data_s1_df1 = df_all_1.drop(df_all_1.index[0]).reset_index(drop=True)
     return data_s1_df1
 
+
 # All Verifications
 
 
 def s1_df2(dt=df_sheet1):
-    df_all_2 = df_sheet1.iloc[2:34, 11:].drop(
-        [3, 4]).reset_index(drop=True)
+    df_all_2 = df_sheet1.iloc[2:34, 11:].drop([3, 4]).reset_index(drop=True)
     df_all_2.columns = df_all_2.iloc[0]
     data_s1_df2 = df_all_2.drop(df_all_2.index[0]).reset_index(drop=True)
     data_s1_df2['Alignment Score'] = data_s1_df2['Alignment Score'].astype(int)
     return data_s1_df2
+
 
 # Statistics Across First 31 Verifications
 
@@ -39,9 +43,10 @@ def s1_df4(dt=df_sheet1):
 def s1_df5(dt=df_sheet1):
     df_stat_3 = dt.iloc[55:61, 1:10].reset_index(drop=True)
     df_stat_3.columns = df_stat_3.iloc[0]
-    data_s1_df5 = df_stat_3.drop(df_stat_3.index[0]).drop([
-        3]).reset_index(drop=True)
+    data_s1_df5 = df_stat_3.drop(df_stat_3.index[0]).drop(
+        [3]).reset_index(drop=True)
     return data_s1_df5
+
 
 # Analysis (P4) - Sheet#2
 
@@ -53,6 +58,7 @@ def s2_df1(dt=df_sheet2):
     df_p4_all.columns = df_p4_all.iloc[0]
     data_s2_df1 = df_p4_all.drop(df_p4_all.index[0]).reset_index(drop=True)
     return data_s2_df1
+
 
 # Principle 4 Statistics Across First 30 Verifications
 
--- ImportData.py
+++ ImportData.py
@@ -2,10 +2,16 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
+dataSource = "local"
+
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive'
+]
 creds = ServiceAccountCredentials.from_json_keyfile_name(
-    'client_secret.json', scope)
+    # 'client_secret.json',
+    'client_secret_856697970476-3lhgkaen7h5e1i9873reeenvphjfau69.apps.googleusercontent.com',
+    scope)
 client = gspread.authorize(creds)
 
 # 'verif' is the workbook filename
@@ -13,5 +19,12 @@ sheets = client.open('verif')
 sheet_1 = sheets.worksheet("Analysis (all)")
 sheet_2 = sheets.worksheet("Analysis (P4)")
 
-df_sheet1 = pd.DataFrame(sheet_1.get_all_values())
-df_sheet2 = pd.DataFrame(sheet_2.get_all_values())
+if dataSource == "cloud":
+    df_sheet1 = pd.DataFrame(sheet_1.get_all_values())
+    df_sheet2 = pd.DataFrame(sheet_2.get_all_values())
+elif dataSource == "local":
+    df_sheet1 = pd.DataFrame()
+    df_sheet2 = pd.DataFrame()
+else:
+    df_sheet1 = pd.DataFrame()
+    df_sheet2 = pd.DataFrame()
